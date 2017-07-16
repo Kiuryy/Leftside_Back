a877db3474c3690d06f2ae02a3609e820bd34bbb
Philipp <philipp.koenig94@gmail.com>
2017-07-16 23:21:42 +0200

    Updated website urls

--- src/js/lib/jsu.js
+++ src/js/lib/jsu.js
@@ -1,12 +1,13 @@
 /**
- * jsu v1.1.0
+ * jsu v1.1.1
  *
  * Philipp König
  * https://blockbyte.de/
- *
  */
 (() => {
     "use strict";
+
+    let runningXhr = [];
 
     // >>> Global Methods
     let isDefined = "isDefined";
@@ -53,6 +54,8 @@
 
         // >>> Methods
         let delay = "delay";
+        let xhr = "xhr";
+        let cancelXhr = "cancelXhr";
         // <<< Methods
 
         return {
@@ -65,6 +68,65 @@
             [delay]: (t = 0) => {
                 return new Promise((resolve) => {
                     setTimeout(resolve, t)
+                });
+            },
+            /**
+             * Performs an XMLHttpRequest with the given options
+             *
+             * @param {string} url
+             * @param {object} opts
+             * @returns {Promise}
+             */
+            [xhr]: (url, opts = {}) => {
+                return new Promise((resolve, reject) => {
+                    let xhr = new XMLHttpRequest();
+                    let idx = runningXhr.push({url: url, xhr: xhr}) - 1;
+
+                    xhr.open(opts.method || "GET", url, true);
+                    ["load", "error", "timeout", "abort"].forEach((eventName) => {
+                        xhr.addEventListener(eventName, () => {
+                            runningXhr[idx] = null;
+
+                            if (eventName === "load" && xhr.status < 400) {
+                                resolve(xhr);
+                            } else {
+                                reject(xhr);
+                            }
+                        });
+                    });
+
+                    let formData = null;
+                    if (opts.data) { // submit request parameters
+                        formData = new FormData();
+                        Object.entries(opts.data).forEach(([key, value]) => {
+                            if (typeof value === "object") {
+                                value = JSON.stringify(value);
+                            }
+
+                            formData.append(key, value);
+                        });
+                    }
+
+                    Object.entries(opts).forEach(([key, value]) => { // set specific variables
+                        if (key !== "method" && key !== "data") {
+                            xhr[key] = value;
+                        }
+                    });
+
+                    xhr.send(formData);
+                });
+            },
+            /**
+             * Cancels all XMLHttpRequests or only the ones with the given url
+             *
+             * @param {string} url
+             */
+            [cancelXhr]: (url = null) => {
+                runningXhr.forEach((obj) => {
+                    if (obj && obj.xhr && obj.xhr.readyState && obj.xhr.abort && (url === null || obj.url === url)) {
+                        obj.xhr.abort();
+                        obj = null;
+                    }
                 });
             }
         };
